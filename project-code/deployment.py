1357bd6bc905e07d9c64faa57cf977674de1f503
Gregor von Laszewski <laszewski@gmail.com>
2018-09-22 06:24:20 -0400

    Update deployment.py

--- project-code/deployment.py
+++ project-code/deployment.py
@@ -1,10 +1,27 @@
-class deployment    
-    def prepare
-	    prepares installation including downloads and other installs needed
-   def deploy
-         deployes the package or software
-    def configure
-         does som configurations
-    def test(test)
-         does a test wheer a name is passed of a test (you could have multiple)
-         the name all woudl be running all tests
+from abc import ABC, abstractmethod
+ 
+class AbstractDeplyment(ABC):
+ 
+     def __init__(self):
+         super().__init__()
+    
+     @abstractmethod
+     def prepare(self):
+	 """prepares installation including downloads and other installs needed"""
+	 pass
+
+    @abstractmethod
+    def deploy
+         """deployes the package or software"""
+	pass
+
+    @abstractmethod	
+    def configure(self)
+        """does som configurations"""
+	pass
+
+    @abstractmethod	
+    def test(test):		
+         """does a test wheer a name is passed of a test (you could have multiple)
+         the name all woudl be running all tests"""
+	pass
