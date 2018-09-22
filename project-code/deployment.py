0e61062246c6a05d651b7cdf719b914f37287e27
Gregor von Laszewski <laszewski@gmail.com>
2018-09-22 06:14:42 -0400

    Create deployment.py

---
+++ project-code/deployment.py
@@ -0,0 +1,10 @@
+class deployment    
+    def prepare
+	    prepares installation including downloads and other installs needed
+   def deploy
+         deployes the package or software
+    def configure
+         does som configurations
+    def test(test)
+         does a test wheer a name is passed of a test (you could have multiple)
+         the name all woudl be running all tests
